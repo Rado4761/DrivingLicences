5059af83c2609beeb77e310cd35a2c2ff8ab707a
BarbieSisa <131451616+BarbieSisa@users.noreply.github.com>
2023-04-21 15:24:00 +0300

    Add files via upload

--- Documents.cs
+++ Documents.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,37 @@ namespace BusinessLayer
 {
     public class Documents
     {
+        [Key]
+        public int IDCard { get; set; }
+        [Required]
+        public bool MedicalDocument { get; set; }
+        [Required]
+        public bool BRC { get; set; }
+        [Required]
+        public bool TheoryExam {get; set;}
+        [Required]
+        public bool PracticalExam { get; set; }
+
+        public bool CheckDocuments (bool MedicalDocument, bool BRC, bool TheoryExam, bool PracticalExam)
+        {
+            bool checkdocument = false;
+            if (MedicalDocument == true && BRC == true && TheoryExam == true && PracticalExam == true)
+                return checkdocument == true;
+            else
+                return checkdocument == false;
+        }
+        private Documents()
+        {
+
+        }
+
+        public Documents(int iDCard, bool medicalDocument, bool bRC, bool theoryExam, bool practicalExam)
+        {
+            IDCard = iDCard;
+            MedicalDocument = medicalDocument;
+            BRC = bRC;
+            TheoryExam = theoryExam;
+            PracticalExam = practicalExam;
+        }
     }
 }
--- DrLicence.cs
+++ DrLicence.cs
@@ -8,5 +8,22 @@ namespace BusinessLayer
 {
     public class DrLicence
     {
+       public int UsersID { get; set; }
+        public OrderStatus LicenceStatus { get; set; }
+        public DateTime DateofSubmition { get; set; }
+        public TypeofOrder Type { get; set; }
+        public List<Documents> Documents { get; set; }
+
+        private DrLicence()
+        {
+            Documents = new List<Documents>();
+        }
+
+        public DrLicence(OrderStatus licenceStatus, DateTime dateofSubmition, TypeofOrder type)
+        {
+            LicenceStatus = licenceStatus;
+            DateofSubmition = dateofSubmition;
+            Type = type;
+        }
     }
 }
